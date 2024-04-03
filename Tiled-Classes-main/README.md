66ce79f394a56533b10a1c292988c02cdda3f3cb
CherryTheTea <165163120+CherryTheTea@users.noreply.github.com>
2024-04-03 09:47:14 -0300

    Add files via upload

---
+++ Tiled-Classes-main/README.md
@@ -0,0 +1,9 @@
+# meu-primeiro-repositorio
+
+# Título 1
+
+#### Titulo 4
+
+Este é meu primeiro repositório
+
+mais uma linha
---
+++ Tiled-Classes-main/aula tiled/terreno1.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="terreno1" tilewidth="16" tileheight="16" tilecount="96" columns="16">
+ <image source="tileset.png" trans="000000" width="256" height="96"/>
+ <wangsets>
+  <wangset name="Grama" type="corner" tile="-1">
+   <wangcolor name="grama" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="agua" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="1" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="2" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="3" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="32" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="33" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="34" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="35" wangid="0,2,0,2,0,2,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
