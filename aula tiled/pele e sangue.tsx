d3e047be50ecf503f6ab0117a264dda6e58b3044
SimcaChambord <165163319+SimcaChambord@users.noreply.github.com>
2024-04-03 09:56:42 -0300

    adiciona pele e sangue

---
+++ aula tiled/pele e sangue.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pele e sangue" tilewidth="16" tileheight="16" tilecount="9" columns="3">
+ <image source="Pele e sangue.png" width="48" height="48"/>
+ <wangsets>
+  <wangset name="Conjunto Sem Nome" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="sang" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="2,2,1,1,1,2,2,2"/>
+   <wangtile tileid="1" wangid="2,2,1,1,1,1,1,2"/>
+   <wangtile tileid="2" wangid="2,2,2,2,1,1,1,2"/>
+   <wangtile tileid="3" wangid="1,1,1,1,1,2,2,2"/>
+   <wangtile tileid="4" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="5" wangid="1,2,2,2,1,1,1,1"/>
+   <wangtile tileid="6" wangid="1,1,1,2,2,2,2,2"/>
+   <wangtile tileid="7" wangid="1,1,1,2,2,2,1,1"/>
+   <wangtile tileid="8" wangid="1,2,2,2,2,2,1,1"/>
+  </wangset>
+  <wangset name="conjunto" type="corner" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="1" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="2" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="7" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="8" wangid="0,2,0,2,0,2,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
